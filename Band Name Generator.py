fc36ef2ec08c69feceebaf4f764923140efd5afb
Jbangsensei <johnjonahc@gmail.com>
2022-02-21 17:29:52 -0800

    Added test file and Band Name Generator

---
+++ Band Name Generator.py
@@ -0,0 +1,4 @@
+print("Welcome to Band Name Generator!")
+city_name = input("What city did you grow up in?")
+pet_name = input("What is the name of your pet?")
+print("Your Band Name is: " + city_name + " " + pet_name)
---
+++ Day 1 Test File.py
@@ -0,0 +1,14 @@
+# use print() function to print out a string
+print("Hello World")
+# use \n to add a new line in the output without having to keep typing print
+print("Hello World!\nHello World!\nHello World!")
+# use "+" to concatenate strings
+print("Hello" + "John")
+
+# input() will get user input in console
+# then print() will print the word "Hello" and user input
+print("Hello " + input("What is your name?"))
+
+# to get string length, use len() function
+s = input("What is your name?")
+print(len(s))
