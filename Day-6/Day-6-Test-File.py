aadeccdd5aefda194f15f5e591a79976cfc3ba7a
Jbangsensei <johnjonahc@gmail.com>
2022-03-09 17:35:30 -0800

    Added Day 6 test file

---
+++ Day-6/Day-6-Test-File.py
@@ -0,0 +1,13 @@
+#How to define a function
+
+def my_function():
+    #Do this
+    print("Hello")
+    #Then do this
+    print("Bye")
+    #Finally do this
+    print("See You Later")
+
+#Calling a function
+
+my_function()
