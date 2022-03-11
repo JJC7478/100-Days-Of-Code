5b32a1f112f0aa16d7aa2d7000751be73fc20a0c
Jbangsensei <johnjonahc@gmail.com>
2022-03-10 21:35:47 -0800

    Added Day-8-Test-File

---
+++ Day-8/Day-8-Test-File.py
@@ -0,0 +1,15 @@
+# #Simple Function
+# def greet():
+#     print("Hello")
+#     print("How are you?")
+#     print("What is your name?")
+
+# greet()
+
+#Function that allows for input
+
+def greet_with_name(name):
+    print(f"Hello {name}")
+    print(f"How are you {name}?")
+
+greet_with_name("John")
