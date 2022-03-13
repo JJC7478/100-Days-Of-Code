38d687100c9629f2e3f6d846f34316646c460e3b
Jbangsensei <johnjonahc@gmail.com>
2022-03-12 16:12:59 -0800

    Cleaned for loop for Grading Program

--- Day-9/Grading-Program.py
+++ Day-9/Grading-Program.py
@@ -13,16 +13,16 @@ student_grades = {}
 #Scores 71-80 == "Acceptable"
 #Scores <=79 == "Fail"
 
-for scores in student_scores:
-    student_grades[scores] = student_scores[scores]
-    if student_grades[scores] >= 91 and student_grades[scores] <= 100:
-        student_grades[scores] = "Outstanding"
-    elif student_grades[scores] >= 81 and student_grades[scores] <= 90:
-        student_grades[scores] = "Exceeds Expectations"
-    elif student_grades[scores] >= 71 and student_grades[scores] <= 80:
-        student_grades[scores] = "Acceptable"
+for student in student_scores: 
+    score = student_scores[student]
+    if score > 90:
+        student_grades[student] = "Outstanding"
+    elif score > 80:
+        student_grades[student] = "Exceeds Expectations"
+    elif score > 70:
+        student_grades[student] = "Acceptable"
     else:
-        student_grades[scores] = "Fail"
+        student_grades[student] = "Fail"
     
 
 print(student_grades)
