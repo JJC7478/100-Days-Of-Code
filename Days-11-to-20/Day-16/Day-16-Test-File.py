dcb8fc0abca0d640b51245efd182f5ac50a728e4
Jbangsensei <johnjonahc@gmail.com>
2022-04-12 21:57:31 -0700

    Added prettytable package to day 16 test file

---
+++ Days-11-to-20/Day-16/Day-16-Test-File.py
@@ -0,0 +1,19 @@
+import turtle
+import another_module 
+import prettytable
+
+print(another_module.another_variable)
+
+#timmy = turtle.Turtle()
+
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color("cyan")
+timmy.forward(100)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
