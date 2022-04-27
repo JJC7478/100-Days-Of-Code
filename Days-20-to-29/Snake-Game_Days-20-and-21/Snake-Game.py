935e4e6623b2498c06edcb6010f630e02a6fb680
Jbangsensei <johnjonahc@gmail.com>
2022-04-26 22:51:57 -0700

    Finished food class, added empty scoreboard class

--- Days-20-to-29/Snake-Game_Days-20-and-21/Snake-Game.py
+++ Days-20-to-29/Snake-Game_Days-20-and-21/Snake-Game.py
@@ -1,6 +1,8 @@
 import turtle as t
 import time
 from snake import Snake
+from food import Food
+
 
 screen = t.Screen()
 screen.setup(width=600, height=600)
@@ -9,6 +11,9 @@ screen.title("Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+
+
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
@@ -21,8 +26,11 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-    
     snake.move()
+
+    #Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.relocate()
     
 
 
