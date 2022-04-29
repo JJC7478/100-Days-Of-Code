be8436a575e09dcda12f01d9880ff63ba60c3b0d
Jbangsensei <johnjonahc@gmail.com>
2022-04-29 09:35:13 -0700

    Added ball class, ball collision, and out of bounds detection

--- Days-20-to-29/Day-22_Pong/pong-main.py
+++ Days-20-to-29/Day-22_Pong/pong-main.py
@@ -1,6 +1,7 @@
 import turtle as t
 import time
 from paddle import Paddle
+from ball import Ball
 
 #Creates 800x600 black screen
 screen = t.Screen()
@@ -11,6 +12,7 @@ screen.tracer(0)
 
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
+ball = Ball()
 
 screen.update()
 
@@ -23,11 +25,32 @@ screen.onkeypress(l_paddle.down, "s")
 
 game_on = True
 while game_on:
+
+    time.sleep(0.1)
     screen.update()
+    ball.move()
 
+    #Detect wall collision
+    if ball.current_y > 290 or ball.current_y < -290:
+        ball.bounce_y()
 
+    #Detect paddle collision
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320:
+        ball.bounce_x()
+        
+    elif l_paddle.distance(ball) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+    
+    #Detect right out of bounds
 
+    if ball.current_x > 400:
+        ball.reset_position()
+        ball.bounce_x()
 
+    #Detect left out of bounds
+    if ball.current_x < -400:
+        ball.reset_position()
+        ball.bounce_x()
 
 
 
