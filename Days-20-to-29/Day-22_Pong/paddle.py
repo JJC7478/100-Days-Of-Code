b9b99fa770abfb890479a4e0bbd8f1f79c280fbe
Jbangsensei <johnjonahc@gmail.com>
2022-04-28 19:23:18 -0700

    Formatted paddle for OOP and created second paddle

--- Days-20-to-29/Day-22_Pong/paddle.py
+++ Days-20-to-29/Day-22_Pong/paddle.py
@@ -1,5 +1,31 @@
-import turtle as t
+from turtle import Turtle
+SPEED = "fastest"
+SHAPE = "square"
+COLOR = "white"
+S_WID = 5
+S_LEN = 1
 
-class Paddle():
-    def __init__():
-        
+
+class Paddle(Turtle):
+    def __init__(self, position):
+        super().__init__()
+        self.penup()
+        self.speed(SPEED)
+        self.shape(SHAPE)
+        self.color(COLOR)
+        self.resizemode("user")
+        self.shapesize(stretch_wid=S_WID, stretch_len=S_LEN)
+        self.goto(position)
+    
+
+    def up(self):
+        """Moves paddle up when pressing up arrow"""
+        new_y = self.ycor()
+        if self.ycor() < 280:
+            self.goto(self.xcor(), new_y + 20)
+    
+    def down(self):
+        """Moves paddle down when pressing down arrow"""
+        new_y = self.ycor()
+        if self.ycor() > -280:
+            self.goto(self.xcor(), new_y - 20)
--- Days-20-to-29/Day-22_Pong/pong-main.py
+++ Days-20-to-29/Day-22_Pong/pong-main.py
@@ -1,37 +1,25 @@
 import turtle as t
 import time
+from paddle import Paddle
 
+#Creates 800x600 black screen
 screen = t.Screen()
 screen.screensize(canvwidth=800, canvheight=600)
 screen.bgcolor("black")
 screen.title("My Pong Game")
 screen.tracer(0)
 
-paddle = t.Turtle()
-paddle.penup()
-paddle.speed("fastest")
-paddle.shape("square")
-paddle.color("white")
-paddle.resizemode("user")
-paddle.shapesize(stretch_wid=5, stretch_len=1)
-paddle.goto(x=350, y=0)
-
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 
 screen.update()
 
 screen.listen()
-def up():
-    new_y = paddle.ycor()
-    if paddle.ycor() < 280:
-        paddle.goto(paddle.xcor(), new_y + 20)
 
-def down():
-    new_y = paddle.ycor()
-    if paddle.ycor() > -280:
-        paddle.goto(paddle.xcor(), new_y - 20)
-
-screen.onkeypress(up, "Up")
-screen.onkeypress(down, "Down")
+screen.onkeypress(r_paddle.up, "Up")
+screen.onkeypress(r_paddle.down, "Down")
+screen.onkeypress(l_paddle.up, "w")
+screen.onkeypress(l_paddle.down, "s")
 
 game_on = True
 while game_on:
