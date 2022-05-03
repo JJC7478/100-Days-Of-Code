5b737540c842e0fd541c89cef0a220dd0cf9cf62
Jbangsensei <johnjonahc@gmail.com>
2022-05-02 23:27:58 -0700

    Created car class

--- Days-20-to-29/Day-23/turtle-cross-main.py
+++ Days-20-to-29/Day-23/turtle-cross-main.py
@@ -1,12 +1,18 @@
 import turtle as t
 import time
 from player import Player
+import random
+from car import Car
 
 screen = t.Screen()
 screen.screensize(canvwidth=500, canvheight=500)
 screen.tracer(0)
 
 torta = Player((0,-220))
+cars = []
+for _ in range(40):
+    new_car = Car()
+    cars.append(new_car)
 
 screen.update()
 
@@ -14,10 +20,11 @@ screen.listen()
 screen.onkeypress(torta.up,"w")
 
 game_is_on = True
-
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    
+
 
 
 
