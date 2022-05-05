163aac4f580e611f500d749e996023c58642e156
Jbangsensei <johnjonahc@gmail.com>
2022-05-04 23:27:23 -0700

    Added text files, modified test file

---
+++ Days-20-to-29/Day-24/Day-24-Test-File.py
@@ -0,0 +1,30 @@
+#Opening files in python
+
+#file = open("my_file.txt")
+
+#Reading files
+
+# contents = file.read()
+# print(contents)
+
+# #Closing files
+
+# file.close()
+
+#Alternative way of opening and closing files
+
+# with open("my_file.txt") as file:
+#     contents = file.read()
+#     print(contents)
+#     #No longer have to remember closing down the files
+
+#Make file writeable
+with open("my_file.txt", mode= "a") as file:
+    file.write("\nNew text.")
+    # "w" is for write, but deletes all the text
+    # "a" is for append
+
+#Creating a new file from scratch
+
+with open("new_file.txt", mode= "w") as file:
+    file.write("New text.")
