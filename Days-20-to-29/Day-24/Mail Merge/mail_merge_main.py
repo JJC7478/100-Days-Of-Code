4f6411ff9a844a56e059d0b7f0f432d2ff2a30d0
Jbangsensei <johnjonahc@gmail.com>
2022-05-06 18:16:06 -0700

    Added comments

--- Days-20-to-29/Day-24/Mail Merge/mail_merge_main.py
+++ Days-20-to-29/Day-24/Mail Merge/mail_merge_main.py
@@ -1,5 +1,9 @@
+#Retrieve names from names file and put into list
+
 with open("./Input/Names/invited_names.txt") as file:
     names = file.readlines()
+
+#Removes spaces in names and put into new list
 
 names1 = []
 
@@ -7,6 +11,7 @@ for name in names:
     new_name = name.replace("\n", "")
     names1.append(new_name)
 
+#Retrieve sample letter, replace name placeholder with name, draft new letter
 for name in names1:
     with open("./Input/Letters/starting_letter.txt", mode="a+") as letter:
         letter.seek(0)
