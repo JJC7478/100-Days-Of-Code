25a88c064b8636a2d4e0f835e7ef3d13b42a5ed2
Jbangsensei <johnjonahc@gmail.com>
2022-05-06 17:59:39 -0700

    Added mail merge project

---
+++ Days-20-to-29/Day-24/Mail Merge/mail_merge_main.py
@@ -0,0 +1,20 @@
+with open("./Input/Names/invited_names.txt") as file:
+    names = file.readlines()
+    print(names)
+
+names1 = []
+
+for name in names:
+    new_name = name.replace("\n", "")
+    names1.append(new_name)
+
+print(names1)
+
+for name in names1:
+    with open("./Input/Letters/starting_letter.txt", mode="a+") as letter:
+        letter.seek(0)
+        txt = letter.read()
+        letter_to_name = txt.replace("[name]", f"{name}")
+        with open(f"./Output/ReadyToSend/letter_for_{name}.txt", mode="a+") as draft:
+            draft.write(letter_to_name)
+
