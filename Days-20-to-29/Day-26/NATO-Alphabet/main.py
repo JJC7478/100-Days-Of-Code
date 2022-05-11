f5e8610d61097ebb7775d0dd8a313d45c37fe931
Jbangsensei <johnjonahc@gmail.com>
2022-05-10 18:52:10 -0700

    Edited code_words variable in main.py

--- Days-20-to-29/Day-26/NATO-Alphabet/main.py
+++ Days-20-to-29/Day-26/NATO-Alphabet/main.py
@@ -10,6 +10,6 @@ nato_dict = {value.letter: value.code for (key,value) in nato_alphabet.iterrows(
 #Creates a list of the phonetic code words from a word that the user inputs
 
 new_word = input("Enter a word: ").upper()
-code_words = [code_word  for letter in new_word for (key,code_word) in nato_dict.items() if letter == key]
+code_words = [code_word for letter in new_word for (key,code_word) in nato_dict.items() if letter == key]
 
 print(code_words)
