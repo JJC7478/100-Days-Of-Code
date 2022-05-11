14d3a75d08191c478e7471677c838c8451419596
Jbangsensei <johnjonahc@gmail.com>
2022-05-10 18:50:05 -0700

    Finished NATO Alphabet file

--- Days-20-to-29/Day-26/NATO-Alphabet/main.py
+++ Days-20-to-29/Day-26/NATO-Alphabet/main.py
@@ -1,27 +1,16 @@
-student_dict = {
-    "student": ["Angela", "James", "Lily"], 
-    "score": [56, 76, 98]
-}
-
-#Looping through dictionaries:
-for (key, value) in student_dict.items():
-    #Access key and value
-    pass
-
+from unicodedata import name
 import pandas
-student_data_frame = pandas.DataFrame(student_dict)
 
-#Loop through rows of a data frame
-for (index, row) in student_data_frame.iterrows():
-    #Access index and row
-    #Access row.student or row.score
-    pass
+#Creates dictionary of letters and corresponding NATO code words
 
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
+nato_alphabet = pandas.read_csv("NATO-Alphabet/nato_phonetic_alphabet.csv")
+nato_dict = {value.letter: value.code for (key,value) in nato_alphabet.iterrows()}
 
-#TODO 1. Create a dictionary in this format:
-{"A": "Alfa", "B": "Bravo"}
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
+#Creates a list of the phonetic code words from a word that the user inputs
+
+new_word = input("Enter a word: ").upper()
+code_words = [code_word  for letter in new_word for (key,code_word) in nato_dict.items() if letter == key]
+
+print(code_words)
