efdf2ccfcd87beb9594a8b53f5fe8b75b6f8b2a7
Jbangsensei <johnjonahc@gmail.com>
2022-05-13 14:42:06 -0700

    Added tkinter widget demo and starting mile to km converter files

--- Days-20-to-29/Day-27/Day-27-Test-File.py
+++ Days-20-to-29/Day-27/Day-27-Test-File.py
@@ -1,23 +1,50 @@
+from re import L
 import tkinter
 import turtle as t
-
 
 window = tkinter.Tk()
 window.title("My First GUI Program")
 window.minsize(width=500, height=500)
 
+#Padding gives more space for widgets
+window.config(padx=20, pady=20)
+
+def button_clicked():
+    my_label.config(text=input.get())
+
 #Label
 
 my_label = tkinter.Label(text="I Am a Label", font=("Arial", 24, "bold"))
-my_label.pack()
 
+#Two ways of changing label text
 my_label["text"] = "New Text"
-my_label.config(text= "New Text")
+my_label.config(text= "New Text", padx=100, pady=100)
 
+#Pack stacks each widget next to each other in a vaugely logical format
+
+# my_label.pack()
+
+#Place places a widget in a specific coordinate of the screen
+
+# my_label.place(x=200, y=0)
+
+#Grid creates rows and columns based on the number of widgets and puts a widget on a specified row and column
+my_label.grid(column=0, row=0)
 
 #Button
 
-button = tkinter.Button(text="Click Me")
-button.pack()
+button = tkinter.Button(text="Click Me", command=button_clicked)
+# button.pack()
+button.grid(column=1, row=2)
+
+#New Button
+new_button = tkinter.Button(text="Click Me Too", command=button_clicked)
+new_button.grid(column=2, row=1)
+
+#Entry
+
+input = tkinter.Entry()
+# input.pack()
+input.grid(column=3, row=3)
 
 window.mainloop()
---
+++ Days-20-to-29/Day-27/mile-to-km-converter.py
@@ -0,0 +1,5 @@
+import tkinter
+
+
+mile = 1
+km = mile * 1.609
