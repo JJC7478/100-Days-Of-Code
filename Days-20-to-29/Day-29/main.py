7954f5d3db7b9e3044679fd0d66efa4c1f479773
Jbangsensei <johnjonahc@gmail.com>
2022-05-14 15:04:53 -0700

    Added window

--- Days-20-to-29/Day-29/main.py
+++ Days-20-to-29/Day-29/main.py
@@ -1,5 +1,31 @@
+from tkinter import *
+
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+#Window
+window = Tk()
+window.title("My Password Manager")
+window.config(width=300, height=300, padx=20)
+
+#Logo
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+window.mainloop()
+
