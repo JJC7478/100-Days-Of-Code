6e3b99c7c89b6b07493b5306ec51c0da4bd0ccbb
Jbangsensei <johnjonahc@gmail.com>
2022-05-15 15:51:59 -0700

    Added venv, pyperclip, and copy to clipboard for password

--- Days-20-to-29/Day-29/main.py
+++ Days-20-to-29/Day-29/main.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import messagebox
 from random import randint, choice, shuffle
+import pyperclip
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
@@ -24,6 +25,7 @@ def generate_password():
     password = "".join(char_list)
 
     pw_entry.insert(0,password)
+    pyperclip.copy(password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
