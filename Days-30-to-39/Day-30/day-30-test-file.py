0036dc197478957a0797e7e1256b85d7293f7958
Jbangsensei <johnjonahc@gmail.com>
2022-05-16 19:46:34 -0700

    Modified test file

---
+++ Days-30-to-39/Day-30/day-30-test-file.py
@@ -0,0 +1,28 @@
+#Catching Exceptions
+
+# try:
+#     file = open("a_file.txt")
+#     a_dictionary = {"key" : "value"}
+#     print(a_dictionary["key"])
+# except FileNotFoundError:
+#     file = open("a_file.txt", mode="w")
+#     file.write("Something")
+# except KeyError as error_message:
+#     print(f"The key {error_message} does not exist")
+# else:
+#     content = file.read()
+#     print(content)
+# finally:
+#     file.close()
+#     print("File was closed")
+#     raise TypeError("big chungus")
+
+#Raising an Exception
+height = float(input("Height: "))
+weight = float(input("Weight: "))
+
+if height > 3.0:
+    raise ValueError("Human height should not be over 3 meters")
+
+bmi = weight/height ** 2
+print(bmi)
