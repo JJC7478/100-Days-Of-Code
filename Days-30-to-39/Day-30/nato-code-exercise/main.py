0848a713b80a3955df78640de71c677e42474275
Jbangsensei <johnjonahc@gmail.com>
2022-05-16 20:04:42 -0700

    Caught key error exception when entering non-letter characters

--- Days-30-to-39/Day-30/nato-code-exercise/main.py
+++ Days-30-to-39/Day-30/nato-code-exercise/main.py
@@ -3,13 +3,16 @@
 
 import pandas
 
-data = pandas.read_csv("nato_phonetic_alphabet.csv")
+data = pandas.read_csv("nato-code-exercise/nato_phonetic_alphabet.csv")
 #TODO 1. Create a dictionary in this format:
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
-print(phonetic_dict)
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
 word = input("Enter a word: ").upper()
-output_list = [phonetic_dict[letter] for letter in word]
-print(output_list)
+try:
+    output_list = [phonetic_dict[letter] for letter in word]
+except KeyError:
+    print("Sorry, letters in the alphabet only")
+else:
+    print(output_list)
