9a572f7aa960bca708405d3cabb0015f2b8f24d7
Jbangsensei <johnjonahc@gmail.com>
2022-05-21 23:30:23 -0700

    Modified test file

--- Days-30-to-39/Day-32/day-32-test-file.py
+++ Days-30-to-39/Day-32/day-32-test-file.py
@@ -1,1 +1,20 @@
 import smtplib
+
+# test_email = "tomagoman72@gmail.com"
+# my_email = "johnjonahc@gmail.com"
+# my_password = "this_is_my_password"
+
+# with smtplib.SMTP("smtp.gmail.com") as connection:
+#     connection.starttls()
+#     connection.login(user=test_email, password=my_password)
+#     connection.sendmail(from_addr=test_email, to_addrs=my_email, msg="Subject:Hello\n\nThis is the body of my email")
+
+# import datetime as dt
+
+# now = dt.datetime.now()
+# year = now.year
+# month = now.month
+# weekday = now.weekday()
+
+# date_of_birth = dt.datetime(month=8, year=2005, day=15, hour=21, minute=8)
+# print(date_of_birth)
