ea620acb4e97d90f9ba71643818d1682f7ca82bb
Jbangsensei <johnjonahc@gmail.com>
2022-06-03 14:21:12 -0700

    Finished main.py

--- Days-30-to-39/Day-36/stock-news-alert-project/main.py
+++ Days-30-to-39/Day-36/stock-news-alert-project/main.py
@@ -1,5 +1,7 @@
 import os
 import requests
+from twilio.rest import Client
+import html
 
 STOCK_NAME = "TSLA"
 COMPANY_NAME = "Tesla Inc"
@@ -9,6 +11,17 @@ NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
 AV_API_KEY = os.environ.get("AV_API_KEY")
 NEWS_API_KEY = os.environ.get("NEWS_API_KEY")
+
+ACCT_SID = "AC6801ee7852237e37d558bd7618c33190"
+AUTH_TOKEN = os.environ.get("AUTH_TOKEN")
+
+TWIL_NUMBER = os.environ.get("TWIL_NUMBER")
+MY_NUMBER = os.environ.get("MY_NUMBER")
+
+client = Client(ACCT_SID, AUTH_TOKEN)
+
+UP_SYMBOL = "🔺"
+DOWN_SYMBOL = "🔻"
 
 stock_params = {
     "function":"TIME_SERIES_DAILY",
@@ -26,57 +39,40 @@ stock_response.raise_for_status()
 stock_data = stock_response.json()
 daily_stock_data = stock_data["Time Series (Daily)"]
 daily_list = [{day: value} for (day,value) in daily_stock_data.items()]
+  
 
-news_response = requests.get(url=NEWS_ENDPOINT, params=news_params)
-news_response.raise_for_status()
-news_data = news_response.json()
-print(news_data)
-
-    ## STEP 1: Use https://www.alphavantage.co/documentation/#daily
-# When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
-
-#TODO 1. - Get yesterday's closing stock price. Hint: You can perform list comprehensions on Python dictionaries. e.g. [new_value for (key, value) in dictionary.items()]
 yesterday = [daily_list[0].get(key) for key in daily_list[0]]
 yesterday_close = float(yesterday[0]["4. close"])
-#TODO 2. - Get the day before yesterday's closing stock price
 two_day = [daily_list[1].get(key) for key in daily_list[1]]
 two_day_close = float(two_day[0]["4. close"])
-#TODO 3. - Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20. Hint: https://www.w3schools.com/python/ref_func_abs.asp
-diff = round((abs(yesterday_close - two_day_close)),2)
-#TODO 4. - Work out the percentage difference in price between closing price yesterday and closing price the day before yesterday.
-percent_diff = round((((yesterday_close-two_day_close)/yesterday_close) * 100),2)
-#TODO 5. - If TODO4 percentage is greater than 5 then print("Get News").
+diff = round((yesterday_close - two_day_close),2)
+abs_diff = abs(diff)
+percent_diff = round(abs(((yesterday_close-two_day_close)/yesterday_close) * 100),2)
+
+
 if percent_diff > 5:
+    if diff > 0:
+        percent_message = f"{STOCK_NAME}: {UP_SYMBOL}{percent_diff}%"
+    else:
+        percent_message = f"{STOCK_NAME}: {DOWN_SYMBOL}{percent_diff}%"
     news_response = requests.get(url=NEWS_ENDPOINT, params=news_params)
     news_response.raise_for_status()
     news_data = news_response.json()
-    
+    news_articles = news_data["articles"]
+    top_three = news_articles[:3]
+    top_articles = [{"title": article["title"], "brief": article["description"]} for article in top_three]
+    for article in top_articles:
+        message = client.messages \
+                        .create(
+                            body=f"""
+                            {percent_message} 
+Headline: {html.unescape(article["title"])} 
+Brief: {html.unescape(article["brief"])}""",
+                            from_=TWIL_NUMBER,
+                            to=MY_NUMBER
+                        )
 
-    ## STEP 2: https://newsapi.org/ 
-    # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
-
-#TODO 6. - Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
-
-#TODO 7. - Use Python slice operator to create a list that contains the first 3 articles. Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
-
-
-    ## STEP 3: Use twilio.com/docs/sms/quickstart/python
-    #to send a separate message with each article's title and description to your phone number. 
-
-#TODO 8. - Create a new list of the first 3 article's headline and description using list comprehension.
-
-#TODO 9. - Send each article as a separate message via Twilio. 
-
+        print(message.status)
 
 
-#Optional TODO: Format the message like this: 
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
 
