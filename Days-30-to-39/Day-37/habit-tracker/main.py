85960fdf1ca8a3c58429c65205aab3c3f0f08b48
Jbangsensei <johnjonahc@gmail.com>
2022-06-05 23:52:57 -0700

    Added pixel to graph

--- Days-30-to-39/Day-37/habit-tracker/main.py
+++ Days-30-to-39/Day-37/habit-tracker/main.py
@@ -4,6 +4,7 @@ import os
 pixela_endpoint = "https://pixe.la/v1/users"
 PIXELA_TOKEN = os.environ.get("PIXELA_TOKEN")
 PIXELA_USERNAME = os.environ.get("PIXELA_USERNAME")
+GRAPH_ID = "graph1"
 
 user_params = {
     "token": PIXELA_TOKEN,
@@ -18,7 +19,7 @@ user_params = {
 graph_endpoint = f"{pixela_endpoint}/{PIXELA_USERNAME}/graphs"
 
 graph_config = {
-    "id": "graph1",
+    "id": GRAPH_ID,
     "name": "Coding Graph",
     "unit": "Hours",
     "type": "float",
@@ -29,6 +30,16 @@ headers = {
     "X-USER-TOKEN": PIXELA_TOKEN
 }
 
-response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
 
+# print(response.text)
+
+g1_endpoint = f"{graph_endpoint}/{GRAPH_ID}"
+
+pixel_data = {
+    "date": "20220605",
+    "quantity": "1.5"
+}
+
+response = requests.post(url=g1_endpoint, json=body, headers=headers)
 print(response.text)
