418139dccf046a5938a1a121ba47866899d93d37
Jbangsensei <johnjonahc@gmail.com>
2022-06-06 00:08:03 -0700

    Added pixel update functionality

--- Days-30-to-39/Day-37/habit-tracker/main.py
+++ Days-30-to-39/Day-37/habit-tracker/main.py
@@ -1,5 +1,7 @@
+from time import strftime
 import requests
 import os
+from datetime import datetime
 
 pixela_endpoint = "https://pixe.la/v1/users"
 PIXELA_TOKEN = os.environ.get("PIXELA_TOKEN")
@@ -36,10 +38,22 @@ headers = {
 
 g1_endpoint = f"{graph_endpoint}/{GRAPH_ID}"
 
+today = datetime.now()
+
 pixel_data = {
-    "date": "20220605",
+    "date": today.strftime("%Y%m%d"),
     "quantity": "1.5"
 }
 
-response = requests.post(url=g1_endpoint, json=pixel_data, headers=headers)
+
+# response = requests.post(url=g1_endpoint, json=pixel_data, headers=headers)
+# print(response.text)
+
+g1_update_endpoint = f"{g1_endpoint}/20220605"
+
+pixel_update = {
+    "quantity": "1.2"
+}
+
+response = requests.put(url=g1_update_endpoint,json=pixel_update, headers=headers)
 print(response.text)
