68a003dea6ba44c05156b54cf541807a91144bdc
Jbangsensei <johnjonahc@gmail.com>
2022-06-07 12:52:46 -0700

    Sent post request

--- Days-30-to-39/Day-38/workout-tracker/main.py
+++ Days-30-to-39/Day-38/workout-tracker/main.py
@@ -2,5 +2,28 @@ import requests
 import os
 
 APP_ID = os.environ.get("APP_ID")
-NUTRITION_API_KEY = os.environ.get("NUTRITION_API_KEY")
+API_KEY = os.environ.get("API_KEY")
+
+#TODO Using the Nutritionix "Natural Language for Exercise" API Documentation, 
+# figure out how to print the exercise stats for a plain text input.
+
+endpoint = "https://trackapi.nutritionix.com/v2"
+exercise_endpoint = f"{endpoint}/natural/exercise/"
+
+headers = {
+    "x-app-id": APP_ID,
+    "x-app-key": API_KEY
+}
+
+exercise_params = {
+    "query": "ran 3 miles",
+    "gender": "male",
+    "weight_kg": 104,
+    "height_cm": 175,
+    "age": 21
+}
+
+response = requests.post(url=exercise_endpoint, json=exercise_params, headers=headers)
+result = response.json()
+print(result)
 
